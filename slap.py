d1f769b02d42b7f6d67718cc972916c2915d8642
Simounet <contact@simounet.net>
2016-01-26 22:46:34 +0100

    Specific slap selection handler

--- slap.py
+++ slap.py
@@ -5,30 +5,36 @@ from random import choice
 import hexchat
 
 __module_name__ = 'Slap'
-__module_version__ = '2.0'
+__module_version__ = '2.1'
 __module_description__ = 'Slaps specified users'
 __author__ = 'Douglas Brunal (AKA) Frankity'
 
-slaps = [
-    'slaps {} around a bit with a large trout',
-    'gives {} a clout round the head with a fresh copy of WeeChat',
-    'slaps {} with a large smelly trout',
-    'breaks out the slapping rod and looks sternly at {}',
-    'slaps {}\'s bottom and grins cheekily',
-    'slaps {} a few times',
-    'slaps {} and starts getting carried away',
-    'would slap {}, but is not being violent today',
-    'gives {} a hearty slap',
-    'finds the closest large object and gives {} a slap with it',
-    'likes slapping people and randomly picks {} to slap',
-    'dusts off a kitchen towel and slaps it at {}'
-]
+slaps = {
+    'trout': 'slaps {} around a bit with a large trout',
+    'WeeChat': 'gives {} a clout round the head with a fresh copy of WeeChat',
+    'smelly trout': 'slaps {} with a large smelly trout',
+    'sternly': 'breaks out the slapping rod and looks sternly at {}',
+    'bottom': 'slaps {}\'s bottom and grins cheekily',
+    'times': 'slaps {} a few times',
+    'carried away': 'slaps {} and starts getting carried away',
+    'not violent': 'would slap {}, but is not being violent today',
+    'hearty': 'gives {} a hearty slap',
+    'large object': 'finds the closest large object and gives {} a slap with it',
+    'randomly picks': 'likes slapping people and randomly picks {} to slap',
+    'kitchen towel': 'dusts off a kitchen towel and slaps it at {}'
+}
 
 
 def slap_cb(word, word_eol, userdata):
-    if len(word) > 1:
-        nick = word[1]
-        hexchat.command('me ' + choice(slaps).format(nick))
+    wordLen = len(word)
+    if wordLen > 0:
+        if wordLen == 3:
+            nick = word[2]
+            slapSelected = slaps[word[1]]
+        else:
+            nick = word[1]
+            slapSelected = slaps[choice(slaps.keys())]
+        hexchat.command('me ' + slapSelected.format(nick))
     else:
         hexchat.command('help slap')
     return hexchat.EAT_ALL
@@ -37,6 +43,9 @@ def slap_cb(word, word_eol, userdata):
 def unload_cb(userdata):
     print(__module_name__, 'version', __module_version__, 'unloaded.')
 
-hexchat.hook_command('slap', slap_cb, help='SLAP <nick>')
+hexchat.hook_command('slap', slap_cb, help=("Random slap:\n"
+                                            "/slap <nick>\n"
+                                            "Specific slap:\n"
+                                            "/slap <key> <nick>"))
 hexchat.hook_unload(unload_cb)
 print(__module_name__, 'version', __module_version__, 'loaded.')
